bf4c81da898d3b49a98e0653ad886af9da5634de
HarishReddyValluru <valluruharishreddy@hotmail.com>
2017-07-04 17:37:57 +0530

    #Included files and comments for scss files

---
+++ SmartBreach.Static/app/modules/modalTabStructure/modal-tab-structure.component.js
@@ -0,0 +1,39 @@
+﻿
+(function () {
+    'use strict';
+
+    angular.module('smartbreachapp.dashboard')
+        .component('modalTabStructure', {
+            templateUrl: '/static/app/modules/modalTabStructure/layout/modal-tab-structure.html',
+            controller: ['$scope', '$state', controller],
+            controllerAs: 'vm',
+            bindings: {
+                modalInstance: '<'
+            }
+        });
+
+    function controller($scope, $state) {
+        var vm = this;
+        vm.cancel = cancel;
+        vm.navigationMode = "horizontal";
+
+        function cancel() {
+            vm.modalInstance.close();
+            $state.go('dashboard.smartitems');
+        }
+
+        vm.menus = [
+            {
+                id: 'formBuilder', name: 'Form Builder', state: 'tabstructure.formbuilder', cssClass: 'active', hasChildMenus: false, toOpenInNewWindow: false
+            },
+            {
+                id: 'formcontrols', name: 'Form Controls', state: 'tabstructure.formcontrols', cssClass: '', hasChildMenus: false, toOpenInNewWindow: false
+            },
+            {
+                id: 'smartitems', name: 'Smart Items', state: 'tabstructure.smartitems', cssClass: '', hasChildMenus: false, toOpenInNewWindow: false
+            }];
+
+
+    }
+
+})();
---
+++ SmartBreach.Static/app/modules/modalTabStructure/modal-tab-structure.route.js
@@ -0,0 +1,52 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('smartbreachapp.dashboard').config(['$stateProvider', '$urlRouterProvider', config]);
+
+    function config($stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+            .state('tabstructure', {
+                url: "/TabStructure",
+                parent: 'dashboard.smartitems',
+                resolve: {
+                    modalInstance: ['$uibModal', function ($uibModal) {
+                        return $uibModal.open(
+                         {
+                             component: 'modalTabStructure',
+                             backdrop: 'static',
+                             size: "lg",
+                         });
+                    }]
+                }
+            })
+            .state('tabstructure.formbuilder',
+            {
+                url: "/FormBuilder",
+                views: {
+                    'tabbody@': {
+                        template: "<dynamic-formbuilder></dynamic-formbuilder>"
+                    }
+                }
+            })
+            .state('tabstructure.formcontrols',
+            {
+                url: "/FormControls",
+                views: {
+                    'tabbody@': {
+                        template: "<form-controls></form-controls>"
+                    }
+                }
+            })
+            .state('tabstructure.smartitems',
+            {
+                url: "/SmartItems",
+                views: {
+                    'tabbody@': {
+                        template: "<smart-items></smart-items>"
+                    }
+                }
+            })
+    }
+})();
